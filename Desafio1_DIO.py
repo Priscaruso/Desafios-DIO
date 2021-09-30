c2d0dbd15c6062095a0e5efa0134f4a20674e3a5
Priscila <83982164+Priscaruso@users.noreply.github.com>
2021-09-30 15:19:36 -0300

    Update Desafio1_DIO.py

--- Desafio1_DIO.py
+++ Desafio1_DIO.py
@@ -1,7 +1,7 @@
 #enter a grade from 0 to 100
 nota = int(input( ))
 
-#convert the numeric grades to the A,B,C,D and E grade format
+#convert the numerical grades to the A,B,C,D and E grade format
 if 0 <= nota < 1:
     print('E')
 elif 1 <= nota <= 35:
