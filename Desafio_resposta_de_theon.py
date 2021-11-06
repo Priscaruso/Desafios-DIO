aaa450840734f8701d3c870ace0180781e4bb4c5
Priscila Caruso <83982164+Priscaruso@users.noreply.github.com>
2021-11-05 21:43:32 -0300

    Update Desafio_resposta_de_theon.py

--- Desafio_resposta_de_theon.py
+++ Desafio_resposta_de_theon.py
@@ -1,21 +1,15 @@
+#enter the number of people
 N = int(input())
+
+# enter the number of times theon can be hitted
 persons = list(input().split())
+
+# the lowest value theon can be hitted
 lowest = 0
 
-
-#for i in range(N):
-#    if i == 0:
-#        lowest = persons[i]
-#        continue
-#   if persons[i] < lowest:
-#       lowest = persons[i]
-#        lowest_pos = i
-#print(lowest_pos + 1)
-
-
+# it guesses theon executioner
 for i in range(N):
     if persons[i] == min(persons):
         lowest = persons[i]
         algoz = persons.index(lowest)
-
 print(algoz + 1)
